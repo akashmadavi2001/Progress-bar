554380cbc3ac5d11379730e5703e05bd948c8f17
Akash Madavi <akashmadavi2001@gmail.com>
2025-02-14 13:46:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let bar = document.getElementById('circular-bar');
+let values = document.getElementById('value');
+let progressValue = 0;
+let progressEnd = 100;
+let speed = 100;
+
+let porgress = setInterval(() => {
+    progressValue++;
+    if (progressValue == progressEnd) {
+        clearInterval(porgress)
+    }
+    values.textContent =`${ (progressValue < 10 ? '0' : '') + progressValue}%`;
+    // values.innerHTML = (progressValue < 10 ? '0' : '') + progressValue + "%";
+    bar.style.background = `conic-gradient(rgb(0, 136, 255) ${progressValue * 3.6}deg, lightblue ${progressValue * 3.6}deg)`;
+}, speed);
