ba60ac34eb7c3a88e312fcac60d135b166ecfad2
Akash Madavi <akashmadavi2001@gmail.com>
2025-08-04 09:21:44 +0530

    script.js

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ btn.addEventListener('click', () => {
     btn.innerHTML = 'Wait...';
     let timeout = setInterval(() => {
         value++;
+        
         if (value == 100) {
             clearInterval(timeout);
             btn.innerHTML = 'Try again';
         };
+        
         num.innerHTML = value + "%";
         circle.style.background = 'conic-gradient(#00b3ff ' + value * 3.6 + 'deg, lightblue ' + value * 3.6 + 'deg)';
     }, 100);
 });
+
